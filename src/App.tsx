35624b6e1971a8ca10e0fa623a9aa31af224b78f
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-09-15 20:21:24 -0300

    aula 3: in progress

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ import { Label } from './components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
 import { Slider } from './components/ui/slider';
 import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
+import { useState } from 'react';
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+
+  function handlePromptSelected(template: string){
+    console.log(template);
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -51,15 +59,9 @@ export function App() {
           <form className='space-y-6'>
           <div className='space-y-2'>
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..."/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+
+              <PromptSelect onPromptSelected={handlePromptSelected}/>
+
             </div>
             <div className='space-y-2'>
               <Label>Modelo</Label>
@@ -84,6 +86,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className='block text-xs text-muted-foreground italic leading-relaxed'>
                 Valores mais altos tendem a deixar os resultados mais criativos e com possíveis erros.
